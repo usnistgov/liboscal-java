eac21cfc3b4accb3c3bc98be8ddc2b8f76a6815a
selenaxiao-nist <142448406+selenaxiao-nist@users.noreply.github.com>
2026-08-13 15:51:00 -0400

    Control promotion handle duplication, check if control is already at top level

--- src/main/java/gov/nist/secauto/oscal/lib/profile/resolver/selection/FilterNonSelectedVisitor.java
+++ src/main/java/gov/nist/secauto/oscal/lib/profile/resolver/selection/FilterNonSelectedVisitor.java
@@ -207,8 +207,11 @@ public class FilterNonSelectedVisitor
       control.setId(entity.getIdentifier());
 
       if (!SelectionStatus.SELECTED.equals(index.getSelectionStatus(parent))) {
-        // promote this control
-        retval.promoteControl(control);
+        // promote this control if control is not already at the top level (control's parent is Catalog)
+        // If already at top level, then promotion is not needed because it was added by Import class
+        if (!(parent.getValue() instanceof Catalog)) {
+          retval.promoteControl(control);
+        }
       }
       childResult.applyTo(control);
     } else {
